780694d6956214fc78f49ebd590f44e765ed6a32
YSilahin <egor.silagin96@gmail.com>
2021-06-18 17:26:01 +0300

    Fixed variables

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("First 10 Happy numbers:");
         for (long num = 1, count = 0; count < 8; num++) {
-            if (happy_num(num)) {
+            if (happyNumber(num)) {
                 System.out.println(num);
                 count++;
             }
@@ -16,18 +16,18 @@ public class Main {
     }
 
     //getting result
-    public static boolean happy_num(long num){
-        long m = 0;
+    public static boolean happyNumber(long num){
+        long newnum = 0;
         int digit = 0;
         HashSet<Long> cycle = new HashSet<Long>();
         while(num != 1 && cycle.add(num)){
-            m = 0;
+            newnum = 0;
             while(num > 0){
                 digit = (int)(num % 10);
-                m += digit*digit;
+                newnum += digit*digit;
                 num /= 10;
             }
-            num = m;
+            num = newnum;
         }
         return num == 1;
     }
